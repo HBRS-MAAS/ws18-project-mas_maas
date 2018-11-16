c86f6135b5b5af1068780a08a80e9452e15db3c6
erickkramer <erickkramer@gmail.com>
2018-11-16 19:47:20 +0100

    Preparation table agent added to start

--- src/main/java/org/mas_maas/Start.java
+++ src/main/java/org/mas_maas/Start.java
@@ -11,6 +11,7 @@ public class Start {
         agents.add("BakingInterface:org.mas_maas.agents.BakingInterface");
         agents.add("DoughManager:org.mas_maas.agents.DoughManager");
         agents.add("KneadingMachineAgent:org.mas_maas.agents.KneadingMachineAgent");
+        agents.add("PreparationTableAgent:org.mas_maas.agents.PreparationTableAgent");
         agents.add("Proofer:org.mas_maas.agents.Proofer");
 
         List<String> cmd = new Vector<>();
