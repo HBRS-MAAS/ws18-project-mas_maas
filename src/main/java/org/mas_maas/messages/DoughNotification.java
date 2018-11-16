1fe61a2cb95c2cc9b62e2bcf8745c732dbe25b36
emassey2 <emassey2@wisc.edu>
2018-11-16 21:44:46 +0100

    removing quantity from dough notification

--- src/main/java/org/mas_maas/messages/DoughNotification.java
+++ src/main/java/org/mas_maas/messages/DoughNotification.java
@@ -5,21 +5,7 @@ import java.util.Vector;
 public class DoughNotification extends DoughMessage {
     private int quantity;
 
-    public DoughNotification(Vector<String> guids, String productType, int quantity) {
+    public DoughNotification(Vector<String> guids, String productType) {
         super(guids, productType);
-        this.quantity = quantity;
-    }
-
-    public int getQuantity() {
-        return quantity;
-    }
-
-    public void setQuantity(int quantity) {
-        this.quantity = quantity;
-    }
-
-    @Override
-    public String toString() {
-        return "DoughNotification [quantity=" + quantity + "]";
     }
 }
