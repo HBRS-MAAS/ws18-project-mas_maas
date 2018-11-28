3db9931d51a7af198211088a41a8bb85c2bc1c49
emassey2 <emassey2@wisc.edu>
2018-11-28 10:59:11 +0100

    adding isAvailable status flag for all equpiment

--- src/main/java/org/mas_maas/objects/Equipment.java
+++ src/main/java/org/mas_maas/objects/Equipment.java
@@ -2,9 +2,16 @@ package org.mas_maas.objects;
 
 abstract public class Equipment {
     private String guid;
+    private boolean isAvailable;
 
     public Equipment(String guid) {
         this.guid = guid;
+        this.isAvailable = true;
+    }
+
+    public Equipment(String guid, boolean isAvailable) {
+        this.guid = guid;
+        this.isAvailable = isAvailable;
     }
 
     public String getGuid() {
@@ -15,8 +22,16 @@ abstract public class Equipment {
         this.guid = guid;
     }
 
+    public boolean isAvailable() {
+        return isAvailable;
+    }
+
+    public void setAvailable(boolean isAvailable) {
+        this.isAvailable = isAvailable;
+    }
+
     @Override
     public String toString() {
-        return "Equipment [guid=" + guid + "]";
+        return "Equipment [guid=" + guid + ", isAvailable=" + isAvailable + "]";
     }
 }
