bf85b2ec119d04d4761136dff1acb273d2ca2c9e
Angela Enriquez <anenriqu@outlook.com>
2018-12-12 18:17:25 +0100

    Starting just the AgentCreator in the start file

--- src/main/java/org/mas_maas/Start.java
+++ src/main/java/org/mas_maas/Start.java
@@ -8,20 +8,23 @@ public class Start {
         JSONConverter.test_parsing();
 
         List<String> agents = new Vector<>();
-        agents.add("TimeKeeper:org.mas_maas.agents.TimeKeeper");
+        
+        agents.add("AgentCreator:org.mas_maas.agents.AgentCreator");
+        
+        //agents.add("TimeKeeper:org.mas_maas.agents.TimeKeeper");
         //agents.add("CoolingRackAgent:org.maas.agents.CoolingRackAgent");
         //agents.add("Intermediater:org.mas_maas.agents.Intermediater");
 
-        agents.add("TimeKeeper:org.mas_maas.agents.TimeKeeper");
-        agents.add("DoughManager:org.mas_maas.agents.DoughManager");
-        agents.add("KneadingMachineAgent:org.mas_maas.agents.KneadingMachineAgent");
-        agents.add("PreparationTableAgent:org.mas_maas.agents.PreparationTableAgent");
-        agents.add("Proofer:org.mas_maas.agents.Proofer");
+        //agents.add("TimeKeeper:org.mas_maas.agents.TimeKeeper");
+        //agents.add("DoughManager:org.mas_maas.agents.DoughManager");
+        //agents.add("KneadingMachineAgent:org.mas_maas.agents.KneadingMachineAgent");
+        //agents.add("PreparationTableAgent:org.mas_maas.agents.PreparationTableAgent");
+        //agents.add("Proofer:org.mas_maas.agents.Proofer");
 
-        agents.add("BakingManager:org.mas_maas.agents.BakingManager");
-        agents.add("OvenAgent:org.mas_maas.agents.OvenAgent");
-        agents.add("BakingPreparationAgent:org.mas_maas.agents.BakingPreparationAgent");
-        agents.add("CoolingAgent:org.mas_maas.agents.CoolingAgent");
+        //agents.add("BakingManager:org.mas_maas.agents.BakingManager");
+        //agents.add("OvenAgent:org.mas_maas.agents.OvenAgent");
+        //agents.add("BakingPreparationAgent:org.mas_maas.agents.BakingPreparationAgent");
+        //agents.add("CoolingAgent:org.mas_maas.agents.CoolingAgent");
 
 //agents.add("PackagingInterfaceAgent:org.mas_maas.agents.PackagingInterfaceAgent");
         List<String> cmd = new Vector<>();
