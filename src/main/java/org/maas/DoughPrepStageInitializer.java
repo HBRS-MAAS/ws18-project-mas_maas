e2bc6d6e130cc47ea94277827067bd5a5b8eb7ca
erickkramer <erickkramer@gmail.com>
2018-12-17 18:57:00 +0100

    Class project folder updated. DoughPrepInitializer agent created.

---
+++ src/main/java/org/maas/DoughPrepStageInitializer.java
@@ -0,0 +1,17 @@
+package org.maas;
+
+import java.util.Vector;
+import org.maas.Initializer;
+// import org.mas_maas.*;
+
+public class DoughPrepStageInitializer extends Initializer {
+    @Override
+    public String initialize() {
+        Vector<String> agents = new Vector<>();
+        agents.add("AgentCreator:org.mas_maas.agents.AgentCreator");
+        agents.add("DummyOrderProcesser:org.mas_maas.agents.DummyOrderProcesser");
+        String agentInitString = String.join(";", agents);
+        agentInitString += ";";
+        return agentInitString;
+    }
+}
