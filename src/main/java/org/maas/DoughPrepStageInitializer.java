71b3f9aacacd60a0ccfcda9a0cb34bbd6403a45f
Angela Enriquez <anenriqu@outlook.com>
2019-01-07 18:51:50 +0100

    Initializing a dummyOrderProcesser agent. The agent receives the scenarioDirectory

--- src/main/java/org/maas/DoughPrepStageInitializer.java
+++ src/main/java/org/maas/DoughPrepStageInitializer.java
@@ -2,14 +2,18 @@ package org.maas;
 
 import java.util.Vector;
 import org.maas.Initializer;
-// import org.mas_maas.*;
+import org.maas.JSONConverter;
+import org.maas.Objects.Bakery;
+import org.maas.Objects.DoughPrepTable;
+import org.maas.Objects.Equipment;
+import org.maas.Objects.Oven;
 
 public class DoughPrepStageInitializer extends Initializer {
     @Override
     public String initialize(String scenarioDirectory) {
         Vector<String> agents = new Vector<>();
-        agents.add("AgentCreator:org.mas_maas.agents.AgentCreator");
-        agents.add("DummyOrderProcesser:org.mas_maas.agents.DummyOrderProcesser");
+        //agents.add("AgentCreator:org.mas_maas.agents.AgentCreator(" + scenarioDirectory + ")");
+        agents.add("DummyOrderProcesser:org.mas_maas.agents.DummyOrderProcesser(" + scenarioDirectory+ ")");
         String agentInitString = String.join(";", agents);
         agentInitString += ";";
         return agentInitString;
