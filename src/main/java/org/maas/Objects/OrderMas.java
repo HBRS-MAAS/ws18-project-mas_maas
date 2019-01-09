b9c1590719fc0b7b7a2839eaa02b3081d74457f1
emassey2 <emassey2@wisc.edu>
2019-01-09 16:09:08 +0100

    fixing up object structure to match that of the json message to correct conversions

--- src/main/java/org/maas/Objects/OrderMas.java
+++ src/main/java/org/maas/Objects/OrderMas.java
@@ -7,25 +7,75 @@ public class OrderMas {
     private String customer_id;
     private String guid;
 
+    public class OrderDate
+    {
+        private int order_day;
+        private int order_hour;
+        public OrderDate(int order_day, int order_hour) {
+            super();
+            this.order_day = order_day;
+            this.order_hour = order_hour;
+        }
+        public int getOrder_day() {
+            return order_day;
+        }
+        public void setOrder_day(int order_day) {
+            this.order_day = order_day;
+        }
+        public int getOrder_hour() {
+            return order_hour;
+        }
+        public void setOrder_hour(int order_hour) {
+            this.order_hour = order_hour;
+        }
+        @Override
+        public String toString() {
+            return "OrderDate [order_day=" + order_day + ", order_hour=" + order_hour + "]";
+        }
+    }
+
+    public class DeliveryDate
+    {
+        private int delivery_day;
+        private int delivery_hour;
+        public DeliveryDate(int delivery_day, int delivery_hour) {
+            super();
+            this.delivery_day = delivery_day;
+            this.delivery_hour = delivery_hour;
+        }
+        public int getDelivery_day() {
+            return delivery_day;
+        }
+        public void setDelivery_day(int delivery_day) {
+            this.delivery_day = delivery_day;
+        }
+        public int getDelivery_hour() {
+            return delivery_hour;
+        }
+        public void setDelivery_hour(int delivery_hour) {
+            this.delivery_hour = delivery_hour;
+        }
+        @Override
+        public String toString() {
+            return "DeliveryDate [delivery_day=" + delivery_day + ", delivery_hour=" + delivery_hour + "]";
+        }
+    }
+
     // I thought about using a standard java object like Calendar or Date
     // but this seemed a better fit given the json format and our limited scope
-    private int order_day;
-    private int order_hour;
-    private int delivery_date;
-    private int delivery_hour;
+    private OrderDate order_date;
+    private DeliveryDate delivery_date;
     private Vector<BakedGood> bakedGoods;
 
     public OrderMas() {}
 
-    public OrderMas(String customer_id, String guid, int order_day, int order_hour, int delivery_date, int delivery_hour,
+    public OrderMas(String customer_id, String guid, int order_day, int order_hour, int delivery_day, int delivery_hour,
             Vector<BakedGood> bakedGoods) {
         super();
         this.customer_id = customer_id;
         this.guid = guid;
-        this.order_day = order_day;
-        this.order_hour = order_hour;
-        this.delivery_date = delivery_date;
-        this.delivery_hour = delivery_hour;
+        this.order_date = new OrderDate(order_day, order_hour);
+        this.delivery_date = new DeliveryDate(delivery_day, delivery_hour);
         this.bakedGoods = bakedGoods;
     }
 
@@ -45,38 +95,6 @@ public class OrderMas {
         this.guid = guid;
     }
 
-    public int getOrder_day() {
-        return order_day;
-    }
-
-    public void setOrder_day(int order_day) {
-        this.order_day = order_day;
-    }
-
-    public int getOrder_hour() {
-        return order_hour;
-    }
-
-    public void setOrder_hour(int order_hour) {
-        this.order_hour = order_hour;
-    }
-
-    public int getDelivery_date() {
-        return delivery_date;
-    }
-
-    public void setDelivery_date(int delivery_date) {
-        this.delivery_date = delivery_date;
-    }
-
-    public int getDelivery_hour() {
-        return delivery_hour;
-    }
-
-    public void setDelivery_hour(int delivery_hour) {
-        this.delivery_hour = delivery_hour;
-    }
-
     public Vector<BakedGood> getBakedGoods() {
         return bakedGoods;
     }
@@ -87,12 +105,7 @@ public class OrderMas {
 
     @Override
     public String toString() {
-        // return "OrderMas [customer_id=" + customer_id + ", guid=" + guid + ", order_date=[" + "day=" + order_day + ", hour="
-        //         + order_hour + "], delivery_date=[" + "day=" + delivery_date + ", hour=" + delivery_hour + "], bakedGoods="
-        //         + bakedGoods + "]";
-
-        return "Order [customer_id=" + customer_id + ", guid=" + guid + ", order_day=" + order_day + ", order_hour="
-                + order_hour + ", delivery_date=" + delivery_date + ", delivery_hour=" + delivery_hour + ", bakedGoods="
-                + bakedGoods + "]";
+        return "OrderMas [customer_id=" + customer_id + ", guid=" + guid + ", orderDate=" + order_date
+                + ", deliveryDate=" + delivery_date + ", bakedGoods=" + bakedGoods + "]";
     }
 }
