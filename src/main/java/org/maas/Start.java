95ad880a11fede4d362dd4fc810f6e10a3e583f4
Angela Enriquez <anenriqu@outlook.com>
2019-01-21 18:22:07 +0100

    The scenarioDirectory is passed in as an argument. The endTime is taken from the meta file

--- src/main/java/org/maas/Start.java
+++ src/main/java/org/maas/Start.java
@@ -15,7 +15,6 @@ public class Start {
     private static boolean customerStage = false;
     private static boolean orderProcessingStage = false;
     private static boolean doughPrepStage = false;
-    private static boolean bakingMasMaas = false;
     private static boolean bakingStage = false;
     private static boolean packagingStage = false;
     private static boolean deliveryStage = false;
@@ -23,7 +22,7 @@ public class Start {
     private static boolean noAgentStarting = true;
     private static boolean runTimeKeeper = true;
 
-    private static String endTime = "000.03.00";
+    private static String endTime = "001.03.00";
     private static String scenarioDirectory = "smaller";
 
     public static void main(String[] args) {
@@ -68,24 +67,17 @@ public class Start {
         if(doughPrepStage) {
             Initializer init = new DoughPrepStageInitializer();
             sb.append(init.initialize(scenarioDirectory));
-            endTime = "004.06.00";
-        }
-        if(bakingMasMaas) {
-            // Initializer init = new DoughPrepStageInitializer();
-            // sb.append(init.initialize(scenarioDirectory));
-			Initializer init = new BakingMasMaasInitializer();
-            sb.append(init.initialize(scenarioDirectory));
-            endTime = "004.06.00";
+            endTime = "002.06.00";
         }
         if(bakingStage) {
-			Initializer init = new BakingStageInitializer();
+            Initializer init = new BakingStageInitializer();
             sb.append(init.initialize(scenarioDirectory));
-            endTime = "004.06.00";
+            endTime = "004.12.00";
         }
         if(packagingStage) {
-			Initializer init = new PackagingStageInitializer();
+            Initializer init = new PackagingStageInitializer();
             sb.append(init.initialize(scenarioDirectory));
-            endTime = "000.11.00";
+            endTime = "003.01.00";
         }
         if(deliveryStage) {
 
@@ -97,7 +89,7 @@ public class Start {
             Initializer boardInit = new BoardVisualisationInitializer(endTime);
             sb.append(boardInit.initialize(scenarioDirectory));
         }
-		if(runTimeKeeper) {
+        if(runTimeKeeper) {
             sb.append("timekeeper:org.maas.agents.TimeKeeper(" + scenarioDirectory + ", " + endTime + ");");
         }
         if(noAgentStarting) {
@@ -127,6 +119,10 @@ public class Start {
                 localPort = args[i+1];
                 ++i;
             }
+            if (args[i].equals("-scenarioDirectory")) {
+                scenarioDirectory = args[i+1];
+                ++i;
+            }
             if (args[i].equals("-customer")) {
                 customerStage = true;
                 noAgentStarting = false;
@@ -137,10 +133,6 @@ public class Start {
             }
             if (args[i].equals("-doughPrep")) {
                 doughPrepStage = true;
-                noAgentStarting = false;
-            }
-            if (args[i].equals("-bakingMasMaas")) {
-                bakingMasMaas = true;
                 noAgentStarting = false;
             }
             if (args[i].equals("-baking")) {
@@ -166,7 +158,6 @@ public class Start {
             if (args[i].equals("-noTK")) { // no TimeKeeper
                 runTimeKeeper = false;
             }
-
         }
         if (!isHost && (port == null || host == null)) {
             System.out.println("instance is not host and host and port have to be specified!");
